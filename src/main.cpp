af28e7267eefe361b641b9b7287baf6456062e30
Goodmush <cherif.brachemi@gmail.com>
2026-08-11 22:53:37 +0200

    Exo4: cercle

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <chrono>
 
 const int WIDTH = 40;
-const int HEIGHT = 20;
+const int HEIGHT = 40;
 
 void clearScreen()
 {
@@ -15,16 +15,28 @@ int main()
 {
 	int frame = 0;
 
+	int centreX = WIDTH / 2;
+	int centreY = HEIGHT / 2;
+	
+
 	while (true)
 	{
 		std::string buffer;
 		buffer.reserve((WIDTH + 1) * HEIGHT);
+		
+		
+		int rayonExt = 16;
+		int rayonInt = 13;
 
 		for (int y = 0; y < HEIGHT; y++)
 		{
 			for (int x = 0; x < WIDTH; x++)
 			{
-				bool on = (x + y) % 8 == 0;
+				int dx = x-centreX;
+				int dy = (y-centreY) * 2;
+				int distanceXY = dx * dx + dy * dy;
+				bool on =  (rayonInt * rayonInt <= distanceXY) && (distanceXY <= rayonExt * rayonExt);
+
 				buffer += on ? '#' : ' ';
 			}
 			buffer += '\n';
