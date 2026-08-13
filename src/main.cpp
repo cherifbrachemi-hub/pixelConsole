17a45bdc95f15058189c33b54ab7e5ab40faff9c
Goodmush <cherif.brachemi@gmail.com>
2026-08-13 20:31:25 +0200

    Exo : rectangle bordures

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <cmath>
 
 const int WIDTH = 40;
-const int HEIGHT = 40;
+const int HEIGHT = 20;
 
 void clearScreen()
 {
@@ -16,33 +16,21 @@ int main()
 {
 	int frame = 0;
 
-	int centreX = WIDTH / 2;
-	int centreY = HEIGHT / 2;
-	
-
 	while (true)
 	{
 		std::string buffer;
 		buffer.reserve((WIDTH + 1) * HEIGHT);
-	
 
-		for (int x = 0; x < WIDTH; x++)
+		for (int y = 0; y < HEIGHT; y++)
 		{
-			float valeurSin1 = sin(x * 0.3 + frame * 0.1);
-			float t1 = (valeurSin1 + 1.0) / 2.0;
-			int hauteurVague1 = (HEIGHT / 4) + t1 * (HEIGHT / 2);
-
-			float valeurSin2 = sin(x * 0.3 + frame * 0.1 + 9.5);
-			float t2 = (valeurSin2 + 1.0) / 2.0;
-			int hauteurVague2 = (HEIGHT / 4) + t2 * (HEIGHT / 2);
-
-			for (int y = 0; y < HEIGHT; y++)
+			
+			for (int x = 0; x < WIDTH; x++)
 			{
+
+				bool on = (x == 0 || y == 0) || (x == 39 || y == 19);
+
+				//find another solution without testing every character
 				
-
-
-				bool on = (y==hauteurVague1) || (y == hauteurVague2);
-
 				buffer += on ? '#' : ' ';
 			}
 			buffer += '\n';
