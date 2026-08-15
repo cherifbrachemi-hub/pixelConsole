b9e633caa5fc8953ebb7f54296a676e73b59b0ef
Goodmush <cherif.brachemi@gmail.com>
2026-08-15 20:12:25 +0200

    Exo rectangle with functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,59 @@
 #include<iostream>
 
 
+const int screenL{ 80 };
+const int screenH{ 20 };
 
-int main()
-{
-	
-	const int screenL{ 80 };
-	const int screenH{ 20 };
+char grid[screenH][screenL];
 
-	char grid[screenH][screenL];
-
-	int rectLength{40};
-	int rectHeight{10};
-
-	int dx{ (screenL - rectLength) / 2 };
-	int dy{ (screenH - rectHeight) / 2 };
+bool condition(int i, int j) {
+	if ((i == j) || (j == (screenL - 1 - i))) {
+		return true;
+	}
+	else
+		return false;
+}
 
 
-	for (int i = 0; i <= (screenH-1); i++) {
-		
-		for (int j = 0; j <= (screenL-1); j++) {
-			if ((i == j) || (j == (screenL - 1 - i))){
+void fillGrid() {
+	for (int i = 0; i <= (screenH - 1); i++) {
+		for (int j = 0; j <= (screenL - 1); j++) {
+			
+			bool on{condition(i,j)};
+
+			if (on) {
 				grid[i][j] = '1';
 			}
 			else
 				grid[i][j] = '0';
 		}
 	}
+}
 
-
-	//Print
-	for (int i = 0; i <= (screenH-1); i++) {
-		for (int j = 0; j <= (screenL-1); j++) {
+void printGrid() {
+	for (int i = 0; i <= (screenH - 1); i++) {
+		for (int j = 0; j <= (screenL - 1); j++) {
 			std::cout << grid[i][j];
 		}
 		std::cout << '\n';
 	}
+}
 
+
+
+int main()
+{
+	
+	int rectLength{40};
+	int rectHeight{10};
+
+	int dx{ (screenL - rectLength) / 2 };
+	int dy{ (screenH - rectHeight) / 2 };
+
+	//fill
+	fillGrid();
+
+	//Print
+	printGrid();
 	return 0;
 }
